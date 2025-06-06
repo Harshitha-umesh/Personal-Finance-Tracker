c5e7b6c114413c83ae893ef94310e8884c4d7faf
Harshitha-umesh <harshitha22kadur@gmail.com>
2025-06-06 21:00:03 +0530

    Add files via upload

---
+++ FrontEnd/expense-tracker/src/contexts/UserContext.jsx
@@ -0,0 +1,39 @@
+import React, { createContext, useState, useEffect } from "react";
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  const updateUser = (userData) => {
+    setUser(userData);
+    localStorage.setItem("user", JSON.stringify(userData));
+  };
+
+  const clearUser = () => {
+    setUser(null);
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem("user");
+    if (storedUser) {
+      setUser(JSON.parse(storedUser));
+    }
+  }, []);
+
+  return (
+    <UserContext.Provider
+      value={{
+        user,
+        updateUser,
+        clearUser,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
